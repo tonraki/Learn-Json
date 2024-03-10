e38386a3f7fd12070520119dde3916390abdb074
tonraki <Keattipong.ng57@snru.ac.th>
2024-03-10 15:49:51 +0700

    change

---
+++ json.js
@@ -0,0 +1,20 @@
+let myfamilyArray = [
+    {
+        name:'ton',
+        old: 29,
+        high: 170,
+    },
+    {
+        name:'non',
+        old: 22,
+        high: 171,
+    },
+    {
+        name:'noina',
+        old: 18,
+        high: 155,
+    }
+]
+myfamilyArray.forEach((myfamily) => {
+    console.log(myfamily.name + ' , ' + myfamily.high)
+})
---
+++ json_part2/json.js
@@ -0,0 +1,8 @@
+let myjson = {
+    name: "ton",
+    age: 29,
+    phoneNumber: 9999999,
+    status: true
+}
+let myobj = JSON.stringify(myjson)
+console.log(typeof myobj)
